ea4a101e60bc14bf40e8ab841743e283b99fcabd
arpitrawat234 <arpitrawat656@gmail.com>
2026-09-03 17:04:46 +0530

    commiting'

--- React/Thinkinginreact/src/App.jsx
+++ React/Thinkinginreact/src/App.jsx
@@ -1,3 +1,24 @@
+import { useState } from 'react';
+
+function FilterableProductTable({ products }) {
+  const [filterText, setFilterText] = useState('fruits');
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  return (
+    <div>
+      <SearchBar
+        filterText={filterText}
+        inStockOnly={inStockOnly} 
+        onFilterTextChange={setFilterText}
+        onInStockOnlyChange={setInStockOnly} />
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly} />
+    </div>
+  );
+}
+
 function ProductCategoryRow({ category }) {
   return (
     <tr>
@@ -22,11 +43,20 @@ function ProductRow({ product }) {
   );
 }
 
-function ProductTable({ products }) {
+function ProductTable({ products, filterText, inStockOnly }) {
   const rows = [];
   let lastCategory = null;
 
   products.forEach((product) => {
+    if ( (product.name.toLowerCase().indexOf(
+        filterText.toLowerCase()
+      ) === -1 ) && (product.price.replace('$', '') !== filterText)
+    ) {
+      return;
+    }
+    if (inStockOnly && !product.stocked) {
+      return;
+    }
     if (product.category !== lastCategory) {
       rows.push(
         <ProductCategoryRow
@@ -55,25 +85,23 @@ function ProductTable({ products }) {
   );
 }
 
-function SearchBar() {
+function SearchBar({ filterText, inStockOnly ,onFilterTextChange,onInStockOnlyChange}) {
   return (
     <form>
-      <input type="text" placeholder="Search..." />
+      <input
+        type="text"
+        value={filterText}
+        placeholder="Search..."
+        onChange={(e)=>onFilterTextChange(e.target.value)}/>
       <label>
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={inStockOnly} 
+          onChange={(e)=>onInStockOnlyChange(e.target.checked)}/>
         {' '}
         Only show products in stock
       </label>
     </form>
-  );
-}
-
-function FilterableProductTable({ products }) {
-  return (
-    <div>
-      <SearchBar />
-      <ProductTable products={products} />
-    </div>
   );
 }
 
